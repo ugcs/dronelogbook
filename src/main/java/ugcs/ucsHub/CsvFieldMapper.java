6fbc44e01ee5f647dfc77f73a4fafdcdd99cb9c1
Amvrosov <amvrosovalex@gmail.com>
2018-08-30 11:20:55 +0800

    closes #3 telemetry grouping by field codes

--- src/main/java/ugcs/ucsHub/CsvFieldMapper.java
+++ src/main/java/ugcs/ucsHub/CsvFieldMapper.java
@@ -4,8 +4,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-import static com.ugcs.ucs.proto.DomainProto.Semantic.*;
-
 public final class CsvFieldMapper {
     private final Map<String, String> typeNameToCsvFieldName;
 
@@ -24,12 +22,12 @@ public final class CsvFieldMapper {
 
     private CsvFieldMapper() {
         Map<String, String> typeNameToCsvFieldName = new HashMap<>();
-        typeNameToCsvFieldName.put(S_LATITUDE.toString(), "fc:latitude");
-        typeNameToCsvFieldName.put(S_LONGITUDE.toString(), "fc:longitude");
-        typeNameToCsvFieldName.put(S_ALTITUDE_AGL.toString(), "cs:altitude_agl");
-        typeNameToCsvFieldName.put(S_GROUND_SPEED.toString(), "fc:ground_speed");
-        typeNameToCsvFieldName.put(S_VOLTAGE.toString(), "fc:main_voltage");
-        typeNameToCsvFieldName.put(S_CURRENT.toString(), "fc:main_current");
+        typeNameToCsvFieldName.put("latitude", "fc:latitude");
+        typeNameToCsvFieldName.put("longitude", "fc:longitude");
+        typeNameToCsvFieldName.put("altitude_agl", "cs:altitude_agl");
+        typeNameToCsvFieldName.put("ground_speed", "fc:ground_speed");
+        typeNameToCsvFieldName.put("main_voltage", "fc:main_voltage");
+        typeNameToCsvFieldName.put("main_current", "fc:main_current");
 
         this.typeNameToCsvFieldName = Collections.unmodifiableMap(typeNameToCsvFieldName);
     }
--- src/main/java/ugcs/ucsHub/TelemetryProcessor.java
+++ src/main/java/ugcs/ucsHub/TelemetryProcessor.java
@@ -1,6 +1,5 @@
 package ugcs.ucsHub;
 
-import com.ugcs.ucs.proto.DomainProto;
 import com.ugcs.ucs.proto.DomainProto.Telemetry;
 import com.ugcs.ucs.proto.DomainProto.Value;
 
@@ -12,14 +11,15 @@ import java.nio.charset.Charset;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.SortedMap;
 import java.util.TreeMap;
 import java.util.stream.Collectors;
 
+import static com.ugcs.common.util.Strings.isNullOrEmpty;
 import static java.util.Comparator.comparing;
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.joining;
@@ -27,15 +27,8 @@ import static java.util.stream.Collectors.toMap;
 
 public class TelemetryProcessor {
     private final List<Telemetry> telemetryList;
-
-    private final static List<String> ALL_TELEMETRY_TYPE_NAMES;
-
-    static {
-        ALL_TELEMETRY_TYPE_NAMES = Arrays.stream(DomainProto.Semantic.values())
-                .map(Enum::toString).collect(Collectors.toList());
-    }
-
     private SortedMap<Long, Map<String, Telemetry>> processedTelemetry = null;
+    private Set<String> allFieldCodes = null;
 
     public TelemetryProcessor(List<Telemetry> telemetryList) {
         this.telemetryList = telemetryList;
@@ -48,7 +41,7 @@ public class TelemetryProcessor {
                     processedTelemetry = telemetryList.stream()
                             .sorted(comparing(Telemetry::getTime))
                             .collect(groupingBy(Telemetry::getTime, TreeMap::new,
-                                    toMap(t -> t.getTelemetryField().getSemantic().toString(), t -> t, (t1, t2) -> {
+                                    toMap(t -> t.getTelemetryField().getCode(), t -> t, (t1, t2) -> {
                                         System.err.println("*** Merge fail:");
                                         System.err.println(t1);
                                         System.err.println(t2);
@@ -60,33 +53,51 @@ public class TelemetryProcessor {
         return processedTelemetry;
     }
 
+    private Set<String> getAllFieldCodes() {
+        if (allFieldCodes == null) {
+            synchronized (this) {
+                if (allFieldCodes == null) {
+                    allFieldCodes = getProcessedTelemetry().values().stream()
+                            .flatMap(m -> m.values().stream())
+                            .map(t -> t.getTelemetryField().getCode())
+                            .collect(Collectors.toSet());
+                }
+            }
+        }
+        return allFieldCodes;
+    }
+
+
     public void printAsCsv(OutputStream out, Charset charset) {
         final PrintWriter writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(out, charset)));
         printCsvHeader(writer);
 
-        final Map<String, String> currentRecord = new HashMap<>();
-        ALL_TELEMETRY_TYPE_NAMES.forEach(typeName -> currentRecord.put(typeName, null));
-
+        final Map<String, Value> currentRecord = new HashMap<>();
+        getAllFieldCodes().forEach(fieldCode -> currentRecord.put(fieldCode, null));
         getProcessedTelemetry().forEach((epochMilli, telemetryMap) -> {
-            telemetryMap.forEach((typeName, telemetry) ->
-                    currentRecord.compute(typeName, (k, v) -> valueToString(telemetry.getValue())));
-            writer.println(convertDateTime(epochMilli) + "," +
-                    ALL_TELEMETRY_TYPE_NAMES.stream()
-                            .map(typeName -> {
-                                final String value = currentRecord.get(typeName);
-                                if (value == null) {
-                                    return "";
-                                }
-                                return value;
-                            })
-                            .collect(joining(",")));
+            telemetryMap.forEach((fieldCode, telemetry) -> {
+                final Value value = telemetry.getValue();
+                if (!isNullOrEmpty(value.toString())) {
+                    currentRecord.put(fieldCode, value);
+                }
+            });
+            final String fieldCodeValues = getAllFieldCodes().stream()
+                    .map(fieldCode -> {
+                        final Value value = currentRecord.get(fieldCode);
+                        if (value == null) {
+                            return "";
+                        }
+                        return valueToString(value);
+                    })
+                    .collect(joining(","));
+            writer.println(convertDateTime(epochMilli) + "," + fieldCodeValues);
         });
         writer.flush();
     }
 
     private void printCsvHeader(PrintWriter writer) {
-        writer.println("Time," + ALL_TELEMETRY_TYPE_NAMES.stream()
-                .map(typeName -> CsvFieldMapper.mapper().convertTypeName(typeName))
+        writer.println("Time," + getAllFieldCodes().stream()
+                .map(fieldCode -> CsvFieldMapper.mapper().convertTypeName(fieldCode))
                 .collect(joining(",")));
     }
 
