4ca148a7bcade3d9245b9f392d14ca2f3eed18e5
Amvrosov <amvrosovalex@gmail.com>
2018-10-06 14:23:51 +0800

    DU-2 Launcher fix for *nix systems

--- src/main/java/ugcs/ucsHub/Launcher.java
+++ src/main/java/ugcs/ucsHub/Launcher.java
@@ -2,10 +2,12 @@ package ugcs.ucsHub;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import static java.lang.Runtime.getRuntime;
 import static java.lang.String.format;
+import static java.nio.file.Files.exists;
 import static ugcs.ucsHub.Settings.settings;
 
 public class Launcher {
@@ -17,10 +19,21 @@ public class Launcher {
             return;
         }
 
-        String javaPath = Paths.get(System.getProperty("java.home"), "bin", "javaw").toString();
-        final String jarPath = new File(Launcher.class.getProtectionDomain().getCodeSource().getLocation().getPath()).toString();
-        final String launchCommand = format("\"%s\" -Xmx%dm -jar \"%s\" ExecutedFromLauncher", javaPath, MAX_HEAP_SIZE_MB, jarPath);
+        final Path javaBinDirPath = Paths.get(System.getProperty("java.home"), "bin");
+        final Path javaBinPath = exists(javaBinDirPath.resolve("javaw"))
+                ? javaBinDirPath.resolve("javaw")
+                : javaBinDirPath.resolve("java");
 
-        getRuntime().exec(launchCommand);
+        final String jarPath =
+                new File(Launcher.class.getProtectionDomain().getCodeSource().getLocation().getPath()).toString();
+
+        final String[] cmdArray = {
+                javaBinPath.toString(),
+                format("-Xmx%dm", MAX_HEAP_SIZE_MB),
+                "-jar", jarPath,
+                "ExecutedFromLauncher"
+        };
+
+        getRuntime().exec(cmdArray);
     }
 }
