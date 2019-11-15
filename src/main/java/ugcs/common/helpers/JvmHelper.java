8731e0101f8f8386e563b4ccf5d00d6eefe1ba2d
Amvrosov <amvrosovalex@gmail.com>
2019-11-15 16:14:48 +0800

    #79 fix for doing the 32-bit check of JVM

--- src/main/java/ugcs/common/helpers/JvmHelper.java
+++ src/main/java/ugcs/common/helpers/JvmHelper.java
@@ -18,6 +18,12 @@ public class JvmHelper {
     }
 
     public static boolean isCurrentJre32Bit() {
+        // Firstly trying to distinguish by platform specific API (https://www.oracle.com/technetwork/java/hotspotfaq-138619.html#64bit_detection)
+        if ("32".equals(System.getProperty("sun.arch.data.model")) || "64".equals(System.getProperty("sun.arch.data.model"))) {
+            return "32".equals(System.getProperty("sun.arch.data.model"));
+        }
+
+        // Or else by trying to make decision based on public property (in case of running on non-HotSpot JVMs)
         return System.getProperty("os.arch").endsWith("86");
     }
 }
